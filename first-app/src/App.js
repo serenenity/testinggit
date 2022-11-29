802fab58aa680e06c9de717fc68a1a1905ada66c
paul michael <mopaul@student.lautech.edu.ng>
2022-11-29 03:19:45 +0100

    getting  ready for CSS

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
 import Employee from './compunent/empolyee'
 
+import { useState } from 'react';
+
 function App() {
+  // we  can write some logic code here
+  //  console.log('we are about to list some employees here');
+
+  const [role, setRole] = useState('dev'); // creating a state
+  const showEmployee = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-        </p>
+      {showEmployee ? 
+        (
+          <>
+
+            <input 
+              type="text"
+              onChange={(e) => {
+                // console.log(e.target.value);
+                setRole(e.target.value); // you never assign directly to a state but you do that through the setRole like a function.
+              }}
+            />
+            <Employee name="Paul" role="CEO"/>
+            <Employee name="TY" role="Co-founder"/>
+            <Employee name="Olubunmi" role={role}/>
+            <Employee />
+            <Employee />
+            <Employee />
+            <Employee />
+          </>
+        )
+      :
+        (
+          <p>you can't see Employees! </p>
+        )
+      }
       
-      </header>
     </div>
   );
 }
--- first-app/src/compunent/empolyee.js
+++ first-app/src/compunent/empolyee.js
@@ -1,6 +1,11 @@
-function Employee(){
+function Employee(props){
     return (
-    <h1> employees </h1>
+        <>
+            <h1> Here is {props.name} </h1>
+            {/* <p>{props.role ? props.role : "No role"}</p> */}
+            {/* the line of code above and below works the same way */}
+            {props.role ? <p>{props.role}</p> : <p>No role</p> }
+        </>
     )
 }
 
